4590726fd3234afc61628b9b9d9bbc492f44abfa
Alfred Cepeda <alfred.cepeda@gmail.com>
2020-08-26 17:38:02 -0400

    Remove declaration parser

--- src/unit-test-resources/DeclarationParser/JSONFromClass/ImportedDefinition.ts
+++
@@ -1,9 +0,0 @@
-import {MyPayload} from "./MyPayload";
-import {MyResult} from "./MyResult";
-import {IRouteHandler} from "../../../types";
-
-export class InterfaceDefinition implements IRouteHandler {
-  handle(payload:MyPayload): MyResult {
-    return { success: true };
-  };
-}
--- src/unit-test-resources/DeclarationParser/JSONFromClass/InlineDefinition.ts
+++
@@ -1,7 +0,0 @@
-import {IRouteHandler} from "../../../types";
-
-export class InlineDefinition implements IRouteHandler {
-  handle(payload:{ firstname:string }): { success:boolean; } {
-    return { success: true };
-  };
-}
